600ae42ea3f320a92913905228c83a27db888b43
alan-s01 <alan.gonzalez128383@gmail.com>
2021-02-08 14:08:42 -0500

    Week 1 0x01 Tasks

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * Alphabets - prints the aplhabets
+ */
+
+int main(void)
+{
+	char letter;
+	for (letter = 'a'; letter <= 'z'; letter++)
+		putchar(letter);
+
+	char letters;
+	for (letters = 'A'; letters <= 'Z'; letters++)
+		putchar(letters);
+	return(0);
+}
