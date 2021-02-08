c3e3311917361ae12e92f1025cf4bc5a8886ba37
alan-s01 <alan.gonzalez128383@gmail.com>
2021-02-08 14:42:31 -0500

    Week 1 0x01 Tasks

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * Alphabets - prints the aplhabets
+ */
+
+int main(void)
+{
+	char letter;
+	for (letter = 'a'; letter <= 'z'; letter++)
+		if (letter == 'e' || letter == 'q')
+		{
+
+		}
+		else
+		{
+			putchar(letter);
+		}
+	return(0);
+}
