97dee85ca00933165d738e3995b28ee7fe09276a
alan-s01 <alan.gonzalez128383@gmail.com>
2021-02-08 15:54:44 -0500

    Week 1 0x01 Tasks

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+
+/**
+ * main - Prints numbers
+ */
+
+int main(void)
+{
+	int num;
+	for (num = 48; num < 58; num++)
+	{
+		putchar(num);
+	}
+	putchar('\n');
+	return(0);
+}
