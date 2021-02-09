520117a8fc8aa2d45c56d0ff8edccc0383d717d8
alan-s01 <alan.gonzalez128383@gmail.com>
2021-02-09 18:39:53 -0500

    Week 1 0x03 Tasks

---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+
+/**
+ * main - Positive or Negative
+ */
+int positive_or_negative(int i)
+{
+	if (i <= -1)
+	{
+		printf("%d is negative\n", i);
+	}
+	else if (i == 0)
+	{
+		printf("%d is zero\n", i);
+	}
+	else
+	{
+		printf("%d is positive\n", i);
+	}
+	return (0);
+}
