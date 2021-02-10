c79a3d111a28f49bb51595c0983aacaf7b719721
alan-s01 <alan.gonzalez128383@gmail.com>
2021-02-09 20:56:42 -0500

    Week 1 0x02 Tasks

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+
+/**
+ * main - prints an absolute value
+ * Return: 0
+ */
+
+int _abs(int a)
+{
+	if (a > 0)
+	{
+		return (a);
+	}
+	else if (a < 0)
+	{
+		a = a * -1;
+		return (a);
+	}
+	else
+	{
+		return (a);
+	}
+}
