52a1ee74b9f6af1761b62878cef7089fafcb871a
alan-s01 <alan.gonzalez128383@gmail.com>
2021-02-09 21:11:41 -0500

    Week 1 0x02 Tasks

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+
+/**
+ * main - prints last number
+ * Return: return las digit
+ **/
+
+int print_last_digit(int n)
+{
+	int last;
+
+	last = n % 10;
+	if (last < 0)
+	{
+		_putchar(-last + 48);
+		return (-last);
+	}
+	else
+	{
+		_putchar(last + 48);
+		return (last);
+	}
+}
