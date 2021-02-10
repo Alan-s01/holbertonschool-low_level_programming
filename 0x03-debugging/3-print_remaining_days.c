f3d88c72b2c0380814cab98f2368de6828cb1024
alan-s01 <alan.gonzalez128383@gmail.com>
2021-02-09 21:58:46 -0500

    Week 1 0x03 Tasks

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -2,12 +2,8 @@
 #include "holberton.h"
 
 /**
- * print_remaining_days - takes a date and prints how many days are
- * left in the year, taking leap years into account
- * @month: month in number format
- * @day: day of month
- * @year: year
- * Return: void
+ * main - prints a date and remaining days
+ * Return: 0
  */
 
 void print_remaining_days(int month, int day, int year)
