5288e14424c1cd2fb83e41e03e8707727a83768a
alan-s01 <alan.gonzalez128383@gmail.com>
2021-02-11 18:44:25 -0500

    Week 2 0x04 Tasks

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,11 @@
+#include "holberton.h"
+
+/**
+ * _isupper - check uppercase character
+ * Return: 0
+ */
+
+int _isupper(int c)
+{
+	return (c >= 'A' && c <= 'Z');
+}
---
+++ 0x04-more_functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+Another functions and loops
