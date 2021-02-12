3167b996d9afc10f7fd7f198c270a598d77913c7
alan-s01 <alan.gonzalez128383@gmail.com>
2021-02-11 19:52:28 -0500

    Week 2 0x04 Tasks

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+/**
+ * print_square - prints a square
+ * Return: void
+ **/
+void print_square(int size)
+{
+	int w;
+	int h;
+
+	if (size <= 0)
+		_putchar('\n');
+	else
+		for (h = 0; h < size; h++)
+		{
+			for (w = 0; w < size; w++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+}
