091f35fb2b1940712b8403e6114eb37b2dbf7a56
alan-s01 <alan.gonzalez128383@gmail.com>
2021-02-11 19:02:11 -0500

    Week 2 0x04 Tasks

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,10 @@
+#include "holberton.h"
+/**
+ * _isdigit - checks digits
+ * Return: 0
+ */
+
+int _isdigit(int c)
+{
+	return (c >= '0' && c <= '9');
+}
---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,9 @@
+#include "holberton.h"
+/**
+ *mul - multiplies two integers
+ *Return: 0.
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
