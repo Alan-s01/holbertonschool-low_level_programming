961ba4e9a41a5c5abace32a09b070f3c3ffeb69f
alan-s01 <alan.gonzalez128383@gmail.com>
2021-02-15 15:57:17 -0500

    Week 3 0x05 Tasks

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+
+/**
+ * reset_to_98 - take a pointer and updates the value
+ *
+ * Return: none
+ */
+
+void swap_int(int *a, int *b)
+{
+	*a = 42;
+	*b = 98;
+}
+
---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+
+/**
+ * _strlen - returns the lenght str
+ *
+ * Return: none
+ */
+
+int _strlen(char *s);
+{
+	*n = 98;
+}
+
