a5fd5c67cc8637b62fcc6ecab40f701fa60d2d5e
alan-s01 <alan.gonzalez128383@gmail.com>
2021-02-15 15:47:12 -0500

    Week 3 0x05 Tasks

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+
+/**
+ * reset_to_98 - take a pointer and updates the value
+ *
+ * Return: none
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+Week 3 Here we go 
