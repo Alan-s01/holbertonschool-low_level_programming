221cfe75b166899c9347fbbfef92d340b0078fe8
alan-s01 <alan.gonzalez128383@gmail.com>
2021-02-15 18:13:14 -0500

    Week 3 0x05 Tasks

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,2 +1,21 @@
+#include "holberton.h"
 
+/**
+ * print_rev - prints a string in reverse
+ *
+ * Return: none
+ */
 
+void print_rev(char *s)
+{
+	int i,c=0;
+	for(i = 0; *(s+i) != '\0'; i++)
+	{
+		c++;
+	}
+	for(i = c - 1; i >= 0; i--)
+	{
+		_putchar(*(s+i));
+	}
+	_putchar('\n');
+}
