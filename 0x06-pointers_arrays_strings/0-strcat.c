a68265c5796f6f447bf05c99728aee5af8d0eb8a
alan-s01 <alan.gonzalez128383@gmail.com>
2021-02-18 16:47:06 -0500

    Week 3 0x06 Tasks

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+
+/**
+ * _strcat - concatenates two strings
+ *
+ * @dest: destiny
+ *
+ * @src: source
+ *
+ * Return: destiny
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int i = 0;
+	int k;
+
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+	for (k = 0; src[k] != '\0'; k++)
+	{
+		dest[i + k] = src[k];
+	}
+	return (dest);
+}
