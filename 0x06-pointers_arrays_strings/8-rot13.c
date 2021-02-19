afcaf716886414f03fc65b8bb6c8ede8025f7ceb
alan-s01 <alan.gonzalez128383@gmail.com>
2021-02-18 21:14:17 -0500

    Week 3 0x06 Tasks

---
+++ 0x06-pointers_arrays_strings/8-rot13.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+/**
+ * rot13 - encodes a string into rot13
+ * @s: array
+ * Return: the array encoded
+ */
+char *rot13(char *s)
+{
+	char a[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	char b[] = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
+	int i, j;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; a[j] != '\0'; j++)
+		{
+			if (s[i] == a[j])
+			{
+				s[i] = b[j];
+				break;
+			}
+		}
+	}
+	return (s);
+}
