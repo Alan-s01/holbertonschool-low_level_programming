a8bde370a41dc834a89fa7aa35aaeab78db3d7fb
alan-s01 <alan.gonzalez128383@gmail.com>
2021-02-22 19:42:22 -0500

    Week 4 0x07 Tasks

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,30 @@
+#include "holberton.h"
+/**
+ * _strspn - gets the lenght of a prefix substring
+ * @s: string
+ * @accept: string
+ * Return: string
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int x = 0;
+	int i;
+
+	while (*s)
+	{
+		for (i = 0; accept[i]; i++)
+		{
+			if (*s == accept[i])
+			{
+				x++;
+				break;
+			}
+			else if (accept[i + 1] == '\0')
+			{
+				return (x);
+			}
+		}
+		s++;
+	}
+	return (x);
+}
