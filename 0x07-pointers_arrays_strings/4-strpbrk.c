0a3b0117d669894b62e897c536095842cd3fc38e
alan-s01 <alan.gonzalez128383@gmail.com>
2021-02-22 19:54:29 -0500

    Week 4 0x07 Tasks

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+/**
+ * _strspn - gets the lenght of a prefix substring
+ * @s: string
+ * @accept: string
+ * Return: string
+ */
+
+char *_strpbrk(char *s, char *accept)
+{
+	int i = 0;
+
+
+}
