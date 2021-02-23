57288d9362d6624f31be85f710099aeba582630f
alan-s01 <alan.gonzalez128383@gmail.com>
2021-02-22 21:33:04 -0500

    Week 4 0x07 Tasks

---
+++ 0x07-pointers_arrays_strings/9-set_string.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+/**
+ * set_string - sets the value of a pointer a char
+ * @s: string
+ * @to: string2
+ * Return: none
+ */
+
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
