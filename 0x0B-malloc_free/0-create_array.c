01d8adfbe9ffe48e19d69528fac5fc856f547260
alan-s01 <alan.gonzalez128383@gmail.com>
2021-03-02 17:34:07 -0500

    Week 5 0x0B Tasks

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+/*
+ * create_array - creates an array of chars
+ * @size: size
+ * @c: char
+ * Return: NULL or array
+ */
+char *create_array(unsigned int size, char c)
+{
+	char *ar;
+	unsigned int i;
+
+	ar = malloc(sizeof(char) * size);
+	if (size == 0)
+		return (NULL);
+	else if (ar == 0)
+		return (NULL);
+	else
+	{
+		for (i = 0; i <= size; i++)
+			ar[i] = c;
+
+		return (ar);
+	}
+}
