924080c866a2b5e9a858848240d18236fca18799
alan-s01 <alan.gonzalez128383@gmail.com>
2021-03-02 23:51:03 -0500

    Week 5 0x0B Tasks

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,36 @@
+#include "holberton.h"
+
+/*
+ * alloc_grid - returns a pointer to 2d
+ * @widht: width
+ * @height: height
+ * Return: NULL or pointer
+ */
+
+int **alloc_grid(int width, int height)
+{
+	int i, i1;
+	int **a;
+
+	if (width <= 0 || height <= 0)
+		return (NULL);
+	a = (int **)malloc(sizeof(int *) * height);
+	if (a == NULL)
+		return (NULL);
+
+	for (i = 0; i < height; i++)
+	{
+		a[i] = (int *)malloc(sizeof(int) * width);
+		if (a[i] == NULL)
+		{
+			for (i1 = 0; i1 < i; i1++)
+				free(a[i1]);
+
+			free(a);
+			return (NULL);
+		}
+		for (i1 = 0; i1 < width; i1++)
+			a[i][i1] = 0;
+	}
+	return (a);
+}
