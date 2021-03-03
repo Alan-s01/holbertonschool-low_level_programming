ffce8d679dd2bc3cf1ece0638142e2abc48d9f25
alan-s01 <alan.gonzalez128383@gmail.com>
2021-03-02 19:33:18 -0500

    Week 5 0x0B Tasks

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,34 @@
+#include "holberton.h"
+
+/*
+ * _strdup - returns a pointer to a newly allocated
+ * @str: string
+ * Return: NULL or 0
+ */
+
+char *_strdup(char *str)
+{
+	unsigned int i = 0;
+	char *nstr;
+
+	if (str == 0)
+		return (0);
+
+	while (str[i] != '\0')
+		i++;
+
+	nstr = malloc(sizeof(char) * (i + 1));
+
+	if (nstr == 0)
+		return (0);
+
+	else
+	{
+		for (i = 0; str[i] != '\0'; i++)
+			nstr[i] = str[i];
+
+		nstr[i] = '\0';
+		return (nstr);
+	}
+}
+
