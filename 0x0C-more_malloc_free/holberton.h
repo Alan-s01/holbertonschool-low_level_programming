818d4f7f08d9bfe96a560cf527728400f9891e3b
alan-s01 <alan.gonzalez128383@gmail.com>
2021-03-04 17:25:41 -0500

    Week 5 0x0C Tasks

---
+++ 0x0C-more_malloc_free/holberton.h
@@ -0,0 +1,12 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
+#include <stdio.h>
+#include <stdlib.h>
+int _putchar(char);
+void *malloc_checked(unsigned int b);
+void *_calloc(unsigned int nmemb, unsigned int size);
+int *array_range(int min, int max);
+
+
+#endif /* HOLBERTON_H */
