f8d810bc58fa3184588c5dba564dbcf322312a18
alan-s01 <alan.gonzalez128383@gmail.com>
2021-03-08 18:48:01 -0500

    Week 6 0x0E Tasks

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,25 @@
+#ifndef DOG_H
+
+#define DOG_H
+
+/**
+* struct dog - new type
+* @name: string
+* @age: float
+* @owner: string
+*/
+
+typedef struct dog
+{
+char *name;
+	float age;
+	char *owner;
+}
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t;
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+
+#endif
