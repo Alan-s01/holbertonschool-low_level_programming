e22e14f76e0ca60974332aba8d2e53d68482ae66
alan-s01 <alan.gonzalez128383@gmail.com>
2021-03-08 19:11:35 -0500

    Week 6 0x0E Tasks

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -11,10 +11,8 @@ void print_dog(struct dog *d)
 	if (d == NULL)
 		return(NULL);
 
-	if (d->name != NULL)
-		printf("Name: %s\n", d->name);
-	printf("Age: %.6f\n", d->age);
-	printf("Owner: %s\n", d->owner);
+	if (d->name == NULL)
+		printf("Name: (nil)\n");
 	else
-		printf("Name: (nil)\n")
+		printf("Name: %s\n", d->name);
 }
