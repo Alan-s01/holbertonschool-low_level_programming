6c07ce01b5815e0575647d3c77bf064fd4bfaf26
alan-s01 <alan.gonzalez128383@gmail.com>
2021-03-11 22:01:29 -0500

    Week 6 0x010 Tasks

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -8,7 +8,7 @@ void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
 
-typedef struct prints
+typedef struct t_prints
 {
 char *val;
 void (*f)(va_list);
