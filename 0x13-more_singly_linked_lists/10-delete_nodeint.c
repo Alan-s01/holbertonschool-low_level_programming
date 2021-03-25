79ffb079161f87be7bdeae57c04e3324710c2ebd
alan-s01 <alan.gonzalez128383@gmail.com>
2021-03-24 22:36:41 -0500

    Week 8 0x013 Tasks

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -1,8 +1,39 @@
 #include "lists.h"
 
+/**
+ * delete_nodeint_at_index - deletes a node at indesx given
+ * @head: list
+ * @index: index
+ * Return: 1
+ */
 
 int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
+	listint_t *t, *c;
+	unsigned int i;
 
+	if (*head == NULL)
+		return (-1);
+
+	c = *head;
+	if (index == 0)
+	{
+		*head = c->next;
+		free(c);
+		return (1);
+	}
+
+	for (i = 0; i < index && c->next; i++)
+	{
+		t = c;
+		c = c->next;
+	}
+
+	if (i < index)
+		return (-1);
+
+	t->next = c->next;
+	free(c);
+	return (1);
 }
 
--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -4,6 +4,7 @@
  * @head: list
  * @idx: index
  * @n: num
+ * Return: node
  */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
