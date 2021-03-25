3f39d6cdf0145145ab0e2301c06c04ae06c67e92
alan-s01 <alan.gonzalez128383@gmail.com>
2021-03-24 22:16:20 -0500

    Week 8 0x013 Tasks

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -6,17 +6,13 @@
  */
 size_t print_listint(const listint_t *h)
 {
-	int i;
 	size_t c;
 
-	if (h)
+	while (h)
 	{
-		for (i = 0; h != NULL; i++)
-		{
-			printf("%d\n", h->n);
-			c++;
-			h = h->next;
-		}
+		printf("%d\n", h->n);
+		c++;
+		h = h->next;
 	}
 	return (c);
 }
