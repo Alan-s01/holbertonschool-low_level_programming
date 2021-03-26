af4f8a6aebc1eb4b3cc265608755205adeb8e8cd
alan-s01 <alan.gonzalez128383@gmail.com>
2021-03-25 23:56:54 -0500

    Week 8 0x014 Tasks

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -1,20 +1,20 @@
 #include "holberton.h"
 
 /**
- * set_bit - sets the value of a bit
+ * clear_bit - sets the value of a bit
  * @n: num
  * @index: index
  * Return: 1 or -1
  */
 
-int set_bit(unsigned long int *n, unsigned int index)
+int clear_bit(unsigned long int *n, unsigned int index)
 {
 	unsigned int i = 1;
 
-	if (index > sizeof(unsigned int) * 8)
+	if (index > sizeof(n) * 8)
 		return (-1);
 
 	i <<= index;
-	*n |= i;
+	*n &= ~i;
 	return (1);
 }
